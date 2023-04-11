692fa872ad8d69d44c60dbe54ff2bde951b79052
susan muraya <susanmuraya89@gmail.com>
2023-04-11 12:26:02 +0300

    logout and login

--- routes/modules/auth.php
+++ routes/modules/auth.php
@@ -6,5 +6,6 @@ use App\Http\Controllers\auth\AuthController;
 Route::group([],function (){
     Route::apiResource('', AuthController::class);
     Route::post('verify/{code}', [AuthController::class, 'verifyAccount']);
+    Route::post('login', [AuthController::class,'login']);
     Route::middleware('auth:api')->post('logout', [AuthController::class,'logout']);
 });
